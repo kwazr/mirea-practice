ebcc9146b55597756949227e827cb31a7a43a098
kwazr <egorzotov02@yandex.ru>
2021-09-12 14:30:21 +0300

    init

--- src/ru/mirea/practice1/ball/Ball.java
+++ src/ru/mirea/practice1/ball/Ball.java
@@ -1,4 +1,34 @@
 package ru.mirea.practice1.ball;
 
 public class Ball {
+    private String color;
+    private int rad;
+
+    public Ball(){}
+    public Ball(String color, int rad){
+        this.color = color;
+        this.rad = rad;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setRad(int rad){
+        this.rad = rad;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getRad(){
+        return rad;
+    }
+
+    public String toString(){
+        return this.color+", radius "+this.rad;
+    }
+
+
 }
--- src/ru/mirea/practice1/ball/BallTest.java
+++ src/ru/mirea/practice1/ball/BallTest.java
@@ -1,4 +1,12 @@
 package ru.mirea.practice1.ball;
 
 public class BallTest {
+    public static void main(String[] args) {
+        Ball b1 = new Ball("Blue", 25);
+        Ball b2 = new Ball();
+        b2.setColor("Red");
+        b2.setRad(13);
+        System.out.println(b1);
+        System.out.println(b2);
+    }
 }

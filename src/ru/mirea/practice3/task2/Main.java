18a9e674b62c47a631da26d3949fef51f35f05ed
kwazr <egorzotov02@yandex.ru>
2021-09-26 20:03:11 +0300

    pr3-4

--- src/ru/mirea/practice3/task2/Main.java
+++ src/ru/mirea/practice3/task2/Main.java
@@ -2,7 +2,7 @@ package ru.mirea.practice3.task2;
 
 public class Main {
     public static void main(String[] args) {
-        MovableRectangle one = new MovableRectangle(1,1,2,4, 6,6);
+        MovableRectangle one = new MovableRectangle(1,1,4,6, 8,8);
         System.out.print(one);
         one.moveDown();
         System.out.print(one);
@@ -12,7 +12,7 @@ public class Main {
         System.out.print(one);
         one.moveUp();
         System.out.print(one);
-        MovableCircle two = new MovableCircle(1,1,4,4,5);
+        MovableCircle two = new MovableCircle(1,1,5,5,7);
         System.out.print(two);
         two.moveDown();
         System.out.print(two);
